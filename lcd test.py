306b151fafa269025762d802d8f4e998a5e373a4
Henning Tuininga <tuiningah@gmail.com>
2013-06-11 01:01:53 +0000

    LCD PLATE IS AWESOME!!

---
+++ lcd test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from Adafruit_CharLCDPlate.Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from time import sleep
+
+
+lcd = Adafruit_CharLCDPlate()
+lcd.clear()
+lcd.message("fffffaaaaddddeee !!!!!")
